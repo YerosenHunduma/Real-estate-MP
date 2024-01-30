57c8343b0c8aac494f3ffe5be2c1e3ad1f262a52
YerosenHunduma <yerosenhunduma@gmail.com>
2024-01-30 15:42:32 +0300

    add redux-persistor to persist the redux state

--- client/src/apiCalls/user.js
+++ client/src/apiCalls/user.js
@@ -12,7 +12,6 @@ export const RegisterUser = async (payload) => {
 export const LoginUser = async (payload) => {
   try {
     const response = await axiosInstance.post("/api/auths/login", payload);
-
     return response.data;
   } catch (error) {
     return error.response.data;
